f8f0b67425c1d87c8797bebeef9c0296c68d913a
v-ashivhare <v-ashivhare@revspringinc.com>
2022-04-02 11:32:43 -0400

    Mobile style fixes

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,13 @@ const search = document.querySelector("input")
 
 const errorContainer = document.querySelector("#error")
 const weather_image = document.querySelector("#weather-image")
+weather_image.setAttribute("style", "display:none;")
 errorContainer.textContent=''
 const weatherContainer = document.querySelector("#location")
 weatherContainer.textContent=''
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    weather_image.setAttribute("style", "display:none;")
     errorContainer.textContent='loading...'
     weatherContainer.textContent=''
     weather_image.src = ''
@@ -29,6 +31,7 @@ weatherForm.addEventListener('submit', (e) => {
                 errorContainer.textContent=''
                 weatherContainer.innerHTML= data.location + '<br>' + data.forecast
                 weather_image.src = textContent=data.url
+                weather_image.setAttribute("style", "display:flex;")
                 console.log(data)
             }
         })
